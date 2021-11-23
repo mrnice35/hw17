81f065b7836fdc6d61a6467e97638f6f9ba259c7
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2021-11-23 18:48:28 +0300

    Добавьте файлы проекта.

---
+++ hw17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apartment<int,int> apartment1 = new Apartment<int,int>();
+            apartment1.Num = 1;
+            int next =apartment1.Num;
+            Apartment<string,int> apartment2 = new Apartment<string,int>();
+            apartment2.Num = "01";
+
+        }
+    }
+    class Apartment<T,U>
+    {
+        public U level { get; set; }
+        public T Num { get; set; }
+    }
+}
---
+++ обобщенные методы/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace обобщенные_методы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print<int>(12);
+            print<string>("Hello");
+            Console.ReadKey();
+        }
+        static void print<T>(T value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
